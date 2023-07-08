3041ee6fe608dc1d8ee45ff8ea998f076c64447a
Ramiro Torres-Escobedo <torresramiro350@gmail.com>
2023-07-08 15:02:46 +0800

    adding aliases for shell

---
+++ aliases.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+alias syncdir="rsync -r -auzvhP"
+alias syncfiles="rsync -auzvhP"
+
+alias ga="git add"
+alias gs="git status"
+alias gcl="git clone"
+alias gcm="git commit -m"
+alias gpp="git pull && git push"
+alias gft="git fetch"
+alias gps="git push"
+alias gpl="git pull"
+alias gaa="git add --all"
+alias gconf="git config --global"
+alias gconflst="git config --global --list"
+alias gtag="git tag"
+alias gnwtag="git tag -a"
